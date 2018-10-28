169ce73cae149258ae346228af0e4dbb6a29439f
rahul desai <rahul909685@gmail.com>
2018-10-28 14:04:44 +0530

    Fist commit

---
+++ src/main/java/com/qa/libs/HomePage.java
@@ -0,0 +1,8 @@
+package com.qa.libs;
+
+public class HomePage {
+
+    public void elements(){
+        System.out.println("no elements");
+    }
+}
---
+++ src/test/java/com/qa/testscripts/HomeTest.java
@@ -0,0 +1,7 @@
+package com.qa.testscripts;
+
+public class HomeTest {
+    public void test(){
+        System.out.println("Go to test");
+    }
+}
